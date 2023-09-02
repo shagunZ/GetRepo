0eb1830ad045e50784bbeffd6686d06caaa57d60
Shagun Mengi <shagunmengi003@gmail.com>
2023-09-02 22:51:05 +0530

    search

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from 'next/link';
 import { FaStar,FaCodeBranch,FaEye } from "react-icons/fa"
 
-
 async function fetchRepos(){
   const response = await fetch('https://api.github.com/users/shagunZ/repos',{
     next:{  //for caching
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,32 @@
+'use client';
+import { useState,useEffect } from 'react';
 import React from 'react'
+import LoadingPage from './loading';
 import Link from 'next/link'
 import Courses from './components/Courses'
+import CourseSearch from './components/CourseSearch';
 const Homepage = () => {
+  const [courses,setCourses] = useState([]);
+  const [loading,setLoading]= useState(true);
+  useEffect(()=>{
+    const fetchCourses  = async()=>{
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data); 
+      console.log("coursess",data)
+      setLoading(false);
+    }
+    fetchCourses();
+  },[]);
+
+  if(loading){
+    return <LoadingPage/>
+  }
   return (
     <div>
       <h1>Welcome</h1>
-      <Courses />
+      <CourseSearch getSearchResults={(results)=>setCourses(results)}/>
+      <Courses courses={courses}/>
     </div>
   )
 }
